70765a2345b50dcd64e81a723dbf064a1dc20d39
Juan Esteban <juanesteban1022@gmail.com>
2026-02-10 09:57:51 -0500

    cambio de backend

--- src/env/enviroment.ts
+++ src/env/enviroment.ts
@@ -2,6 +2,6 @@ const IP = location.hostname;
 
 export const environment = {
     production: false,
-    API_URL: `https://pinceladas-belleza-backend.onrender.com`
+    API_URL: `https://pinceladas-belleza-backend-gwi8.onrender.com`
     // API_URL: `${IP}`
 };
